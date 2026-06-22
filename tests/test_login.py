b53f4d8822a8b429c82f34dcbc1aec23b474cbe9
Steffi <saldanha_steffi@outlook.com>
2026-06-22 14:49:57 +1000

    updated login test to fail

--- tests/test_login.py
+++ tests/test_login.py
@@ -6,16 +6,9 @@ from pages.inventory_page import InventoryPage
 def test_valid_login():
     driver = get_driver()
     driver.get("https://www.saucedemo.com")
-
     login = LoginPage(driver)
-    login.login("standard_user", "secret_sauce")
+    login.login("standard_user", "secret_sauc")
 #   assert "inventory" in driver.current_url
-
     inventory = InventoryPage(driver)
-
     assert inventory.is_loaded()
-
-
-
-
     driver.quit()
