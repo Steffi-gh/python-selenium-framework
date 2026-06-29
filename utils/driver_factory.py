a1cc5d327b4c543b8b705e581a7a7e80e7f8a6f6
Steffi <saldanha_steffi@outlook.com>
2026-06-29 11:55:16 +1000

    test matrix updated for allure reports

--- utils/driver_factory.py
+++ utils/driver_factory.py
@@ -34,6 +34,8 @@ def get_driver(browser="chrome"):
     chrome_options.add_argument("--headless=new")
     chrome_options.add_argument("--no-sandbox")
     chrome_options.add_argument("--disable-dev-shm-usage")
+    chrome_options.add_argument("--disable-gpu")
+
 
     # Disable Chrome password manager + leak detection popup
     chrome_options.add_experimental_option("prefs", {
@@ -71,6 +73,9 @@ def get_driver(browser="chrome"):
         firefox_options.add_argument("--headless")
         firefox_options.add_argument("--width=1920")
         firefox_options.add_argument("--height=1080")
+        firefox_options.set_preference("network.proxy.type", 0)
+        firefox_options.set_preference("browser.tabs.remote.autostart", False)
+
 
         return webdriver.Firefox(
             service=FirefoxService(GeckoDriverManager().install()),
@@ -81,6 +86,8 @@ def get_driver(browser="chrome"):
         edge_options = webdriver.EdgeOptions()
         edge_options.add_argument("--headless=new")
         edge_options.add_argument("--start-maximized")
+        edge_options.add_argument("--disable-gpu")
+
 
         return webdriver.Edge(
             service=EdgeService(EdgeChromiumDriverManager().install()),
